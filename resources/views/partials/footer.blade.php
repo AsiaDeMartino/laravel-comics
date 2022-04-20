2a331a02da37a406e28ebca32a4cc121f281e251
asia_demartino@libero.it <asia_demartino@libero.it>
2022-04-20 03:09:29 +0200

    aggiunta prima parte footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,67 @@
+@php
+    $menu = [
+        [
+            "titolo" => "dc comics",
+            "contenuto" => [
+                "Characters",
+                "Comics",
+                "Movies",
+                "TV",
+                "Games",
+                "Videos",
+                "News"
+            ],
+        ],
+        [
+            "titolo" => "shop",
+            "contenuto" => [
+                "Shop DC",
+                "Shop DC Collectibles"
+            ],
+        ],
+        [
+            "titolo" => "dc",
+            "contenuto" => [
+                "Terms Of Use",
+                "Privacy policy(New)",
+                "Ad Choices",
+                "Advertising",
+                "Subscriptions",
+                "Talent Workshops",
+                "CPSC Certificate",
+                "Rating",
+                "Shop Help",
+                "Contact Us"
+            ],
+        ],
+        [
+            "titolo" => "sites",
+            "contenuto" => [
+                "DC",
+                "MAD Magazine",
+                "DC Kids",
+                "DC Universe",
+                "DC Power Visa",
+            ],
+        ],
+    ]
+@endphp
+
+
+<div>
+    <div class="footer-menu">
+        <div class="menu">
+            @foreach ($menu as $item)    
+                <ul>
+                    <h4>{{$item["titolo"]}}</h4>
+                    @foreach ($item["contenuto"] as $element)
+                    <li> {{ $element }}</li>
+                    @endforeach
+                </ul>
+            @endforeach
+        </div>
+        <div class="logo">
+            <img src="{{ asset('../img/dc-logo-bg.png') }}" alt="">
+        </div>
+      </div>
+    </div>
