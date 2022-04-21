f7e751e3f2d441168b24c7339607179ae8d21a2d
asia_demartino@libero.it <asia_demartino@libero.it>
2022-04-21 03:11:02 +0200

    completato file details

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,5 +1,36 @@
 @extends('layout.home')
 
+@php
+ $menu = [
+        [
+            "href" => "/",
+            "src" => asset("../img/buy-comics-digital-comics.png"),
+            "text" => "digital comics"
+        ],
+        [
+            "href" => "/",
+            "src" => asset("../img/buy-comics-merchandise.png"),
+            "text" => "dc merchandise"
+        ],
+        [
+            "href" => "/",
+            "src" => asset("../img/buy-comics-subscriptions.png"),
+            "text" => "subscription"
+        ],
+        [
+            "href" => "/",
+            "src" => asset("../img/buy-comics-shop-locator.png"),
+            "text" => "comic shop locator"
+        ],
+        [
+            "href" => "/",
+            "src" => asset("../img/buy-dc-power-visa.svg"),
+            "text" => "dc power visa"
+        ],
+    ]
+
+@endphp
+
 @section("main")
 <div class="mainDetails">
     <div class="banner"></div>
@@ -32,6 +63,56 @@
             <figure>
                 <img src="../img/adv.jpg" alt="">
             </figure>
+        </div>
+    </div>
+    <div class="content-bottom">
+        <div class="container">
+            <div class="col-6">
+                <h2>Talent</h2>
+                <div class="talent">
+                    <span>Art by:</span>
+                    <p>
+                        @foreach ($comics["artists"] as $item)
+                            <a href="/">{{ $item }}</a>,
+                        @endforeach
+                    </p>
+                </div>
+                <div class="talent">
+                    <span>Written by:</span>
+                    <p>
+                        @foreach ($comics["writers"] as $item)
+                            <a href="">{{ $item }}</a>
+                        @endforeach
+                    </p>
+                </div>
+            </div>
+            <div class="col-6">
+                <h2>Talent</h2>
+                <div class="talent">
+                    <span>Series:</span>
+                    <p>
+                        <a style="text-transform:uppercase" href="/">{{ $comics["series"] }}</a>
+                    </p>
+                </div>
+                <div class="talent">
+                    <span>U.S. Price:</span>
+                    <p>{{ $comics["price"] }}</p>
+                </div>
+                <div class="talent">
+                    <span>On Sale Date:</span>
+                    <p>{{ $comics["sale_date"] }}</p>
+                </div>
+            </div>
+        </div>
+        <div class="navbar-details">
+            <ul>
+                @foreach ($menu as $item)
+                <li><a href={{ $item["href"] }}></a>
+                 <span>{{ $item["text"]}}</span>
+                 <img src={{ $item["src"] }}>
+                </li>
+                @endforeach
+              </ul>
         </div>
     </div>
 </div>
