85316bd2651f26cc10712689294cb811ceb0fe7c
asia_demartino@libero.it <asia_demartino@libero.it>
2022-04-21 03:20:16 +0200

    aggiunto link alle card

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,13 +5,15 @@
     <div class="banner"></div>
     <button id="current">current series</button>
     <div class="card_wrapper">
-        @foreach ($comics as $item)
-        <div class="card">
-            <figure>
-                <img src={{ $item["thumb"] }} alt="">
-            </figure>
-            <h3>{{ $item["series"] }}</h3>
-        </div>
+        @foreach ($comics as $key=>$item)
+        <a href="/details/{{ $key }}">
+            <div class="card">
+                <figure>
+                    <img src={{ $item["thumb"] }} alt="">
+                </figure>
+                <h3>{{ $item["series"] }}</h3>
+            </div>
+        </a>
         @endforeach
     </div>
     <button>load more</button>
